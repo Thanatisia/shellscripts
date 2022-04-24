3dca67f816e88c21c79a8cdac9fe2861f0607c5c
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2022-04-25 03:15:28 +0800

    Updated program to accept parameters with arguments

--- server/serverctrl/src/main.py
+++ server/serverctrl/src/main.py
@@ -19,6 +19,10 @@ argc = len(argv)
 
 # Variables #
 options = {
+    "Config" : {
+        "description" : "Specify an alternate config path",
+        "arguments" : ["-cfg", "--config"],
+    },
     "Clean" : {
         "description" : "Format Standard Output for Terminal/CLI scripting use",
         "arguments" : ["-c", "--clean"],
@@ -102,47 +106,118 @@ def main():
     # Get Functions & Variables
     if argc > 0:
         # Arguments Provided
-        for i in range(argc):
+        # for i in range(argc):
+        i = 0
+        while i < argc:
             # print("{} : {}".format(i, argv[i]))
             curr_arg = argv[i]
+
+            """
+            Structure:
+                to_run[label] : The name of the function to run
+                to_run[label]["Function"] : The Function Object
+                to_run[label]["Return-Type"] : The Function Return Type
+                    Options:
+                        - void : No Return
+                        - int : Integer return type
+                        - string : String/Text return type
+                        - float/real : Floating point numbers
+                        - bool : Boolean True/False values
+                        - blob : Image/Object type
+                to_run[label]["Input" : The Argument/Parameter to pass into the function
+            """
             if curr_arg == "--help" or curr_arg == "-h":
                 if not ("Help" in to_run):
-                    to_run["Help"] = help_menu
+                    label = "Help"
+                    to_run[label] = {}
+                    to_run[label]["Function"] = help_menu
+                    to_run[label]["Return-Type"] = "void"
+                    to_run[label]["Input"] = None
             elif curr_arg == "--version" or curr_arg == "-v":
                 if not ("Version" in to_run):
-                    to_run["Version"] = version
+                    label = "version"
+                    to_run[label] = {}
+                    to_run[label]["Function"] = version
+                    to_run[label]["Return-Type"] = "void"
+                    to_run[label]["Input"] = None
             elif curr_arg == "--clean" or curr_arg == "-c":
                 if not ("Clean"  in to_run):
                     variables["Clean"] = True
             elif curr_arg == "--cli":
                 if not ("CLI-Mode" in to_run):
-                    to_run["CLI-Mode"] = run_cli
+                    label = "CLI-Mode"
+                    to_run[label] = {}
+                    to_run[label]["Function"] = run_cli
+                    to_run[label]["Return-Type"] = "void"
+                    to_run[label]["Input"] = None
             elif curr_arg == "--gui":
                 if not ("GUI-Mode" in to_run):
-                    to_run["GUI-Mode"] = run_gui
+                    label = "GUI-Mode"    
+                    to_run[label] = {}
+                    to_run[label]["Function"] = run_gui
+                    to_run[label]["Return-Type"] = "void"
+                    to_run[label]["Input"] = None
+            elif curr_arg == "-cfg" or curr_arg == "--config":
+                # Choose Config Path
+                if not ("Config" in to_run):
+                    label = "Config"
+                    to_run[label] = {}
+                    to_run[label]["Function"] = PROG_SETUP.set_prog_config_path
+                    to_run[label]["Return-Type"] = "void"
+
+                    if not ((i+1) >= argc):
+                        to_run[label]["Input"] = argv[i+1]
+        
+                        # Skip next argument
+                        # Next argument is the input to the parameter
+                        i = i+1
+                    else:
+                        # Reached the end, no config path specified
+                        to_run[label]["Input"] = None
+                        print("No Config Path specified.")
             else:
                 print("Invalid Argument [{}]" .format(curr_arg))
-                to_run["Invalid"] = help_menu
+                label = "Invalid"
+                to_run[label] = {}
+                to_run[label]["Function"] = help_menu
+                to_run[label]["Return-Type"] = "void"
+                to_run[label]["Input"] = None
                 break
+
+            # Increment by 1 after each loop
+            i = i+1
 
     else:
         # No Arguments Provided
         # Display Help Menu
         help_menu()
 
+
     # Validation Check - Only run if valid
-    if not ("Invalid" in to_run):
+    if not ("Invalid" in to_run.keys()):
         # print("To Run : ")
         for label, val in to_run.items():
-            # print("\t{} : {}".format(k, v))
+            # print("\t{} : {}".format(label, val))
+            """
             if not ("Clean" in variables.keys()):
                 print("====")
                 print("{}".format(label))
                 print("====")
-            val()
-            
+            """
+
+            func = val["Function"]
+            u_Arg = val["Input"]
+            if(not(u_Arg == None)):
+                # If Argument is not empty
+                func(u_Arg) 
+            else:
+                func()
+
+            """
             if len(to_run) > 1:
-                print("")
+                if not ("Clean" in variables.keys()):
+                    print("")
+            """
 
 if __name__ == "__main__":
     init()
